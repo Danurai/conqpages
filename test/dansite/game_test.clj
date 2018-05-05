786e6dd2c79deff28cc3b7077b0aa18c395d8b63
Danurai <Danurai@users.noreply.github.com>
2018-05-05 13:24:24 +0100

    Add files via upload

--- test/dansite/game_test.clj
+++ test/dansite/game_test.clj
@@ -1,6 +1,7 @@
 (ns dansite.game-test
-  (:require [dansite.misc :refer :all]
-            [dansite.database :as db :refer [users get-authentications rnd-deckid]]
+  (:require [dansite.misc :as misc :refer :all]
+            [dansite.database :as db :refer [users get-user-decks]]
+            [dansite.pages :as pages]
             [clojure.data.json :as json]
             [expectations :refer :all]))
             
@@ -27,3 +28,21 @@
       (take 100)
       count))
         
+(expect "space_marines"
+  (misc/faction-code "010001"))
+  
+(expect #(> 10 %)
+  (-> (db/get-user-decks "1002") count))
+  
+(expect #(> 4 %)
+  (->> (db/get-user-decks "1002")
+        first
+        :data
+        json/read-str
+        (map #(pages/get-card %))
+        first
+        :qty))
+  
+;(expect []
+;  (pages/deck-card {:uid "21CFFD", :name "xx", :author 1002, :data "{\"180043\":1,\"180044\":4,\"180045\":1,\"180046\":2,\"180047\":1}", :tags "astra_militarum", :notes "", :created "2018-05-04T12:42:24.749", :updated "2018-05-04T12:42:24.749"}
+;  ))
